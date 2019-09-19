186c294344179c6ee7065a2cb3f04396aaa2f592
Chayapat Temudomsomboon <chayapatlbs@gmail.com>
2019-09-20 01:41:26 +0700

    add 360 photo

--- BankMatlab/LAB1/DecenteringDistorsion.m
+++ BankMatlab/LAB1/DecenteringDistorsion.m
@@ -1,5 +1,8 @@
 function [distorsionx,distorsiony,Xc,Yc] = DecenteringDistorsion(P1,P2,x,y)
 r = (x^2 + y^2)^0.5;
-
+distorsionx = P1*(r^2 + 2*x^2) + 2*P2*x*y;
+distorsiony = 2*P1*x*y + P2*(r^2 + 2*y^2);
+Xc = x - distorsionx;
+Yc = y - distorsiony;
 end
 
--- BankMatlab/LAB1/Main.m
+++ BankMatlab/LAB1/Main.m
@@ -5,13 +5,15 @@ A = [2442.80 2171.36 ;
      1680.80 2787.16 ;
      859.00  3616.29 ;
      2589.99 2439.53 ;
-     3617.77 3001.40]; % OpenEV cordinates
- A(:,2) = -A(:,2); % Change y to negative cordinates
+     3617.77 3001.40];                 % OpenEV cordinates
+ A(:,2) = -A(:,2);                     % Change y to negative cordinates
  XY(1:length(A),1:2) = 0;
  R(1:length(A)) = 0;
- distorsion(1:length(A),1:2) = 0;%Distortion Value from lens
- XcYc(1:length(A),1:2) = 0;%Value after Corrected Radial lens distortion
- [a,b,c,d] = FindCalibratedFunction();%Find a,b,c,d from Regression
+ distorsion(1:length(A),1:2) = 0;      %Distortion Value from lens
+ XcYc(1:length(A),1:2) = 0;            %Value after Corrected Radial lens distortion
+ distorsion_2(1:length(A),1:2) = 0;    %Distortion Value from Decentering
+ XcYc_2(1:length(A),1:2) = 0;          %Value after Corrected Decentering distortion
+ [a,b,c,d] = FindCalibratedFunction(); %Find a,b,c,d from Regression
  for i = 1:length(A)
      [x,y] = FindRealCordinate(A(i,1),A(i,2),a,b,c,d);
      XY(i,1) = x; XY(i,2) = y;
@@ -23,6 +25,16 @@ A = [2442.80 2171.36 ;
      XcYc(i,1) = Xc          ; XcYc(i,2) = Yc;
      
      %Corrected Decentering lens distortion
-     
+     [del_x,del_y,Xc,Yc] = DecenteringDistorsion(-0.0002739 ,-0.0003423 ,x,y);
+     distorsion_2(i,1) = del_x ; distorsion_2(i,2) = del_y;
+     XcYc_2(i,1) = Xc          ; XcYc_2(i,2) = Yc;
  end
+ %A is input Points (x,y)
+ 
+ %Real (X,Y) cordinates are at XY radius are at R
+ 
+ % Corrected (Xc Yc) are at XcYc(after lens) and XcYc_2
+ 
+ % Distorsions (delx dely) are at distorsion(lens) and
+ % distorsion_2(Decentering)
  
